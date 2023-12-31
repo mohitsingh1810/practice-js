833a87c2e08700f957ea1296a710201bfb8d1a4e
neerajnanvani <neerajnanvaniwork@gmail.com>
2023-12-31 17:11:21 +0530

    practce day 2 done

---
+++ day2.js
@@ -0,0 +1,108 @@
+// max between 3
+// let a = 10;
+// let b = 50;
+// let c = 30;
+
+// if(a>b && a>c){
+//     console.log("a is greater");
+// }
+// else if(b>a && b>c){
+//     console.log("b is greater");
+// }
+// else{
+//     console.log("c is greater");
+// }
+
+//let arr = [1,2,3,4,5];
+// let largest = Number.MAX_VALUE;
+
+// for(let i = 0; i<arr.length;i++){
+//     if(arr[i]>largest){
+//         largest = arr[i]
+//     }
+// }
+// console.log(largest);
+
+
+
+
+// find the specife number in arry
+
+// let arr = [3,5,6,1,10,9,12];
+
+// let number = 2;
+
+
+
+// let count = 0;
+
+// // let check = false;
+
+// for(let i = 0;i<arr.length;i++) {
+//     if(arr[i]== number){
+//         // check = true;
+//         // break; 
+//         count++;
+//     }
+// }
+
+// if(count <= 2) {
+//     console.log("NUMBER 2 BAAR MIL GYA")
+// }
+// if(check){
+//     console.log("number mil gya");   
+// }else{
+//     console.log("number nhi mil ");
+// }
+ 
+
+// let arr = [4, 6, 7, 1, 4, 6, 2, 9, 3, 6, 3, 0, 1, 6,6];
+
+// let num = 6;
+// let count = 0;
+// for(let i = 0;i<arr.length;i++){
+//     if(arr[i] == num){
+//         count++;
+//     }
+
+// }
+// console.log(count);
+
+//replace number in arry
+
+// let arr = [2,4,5,7,1,2,3,7];
+// let num1 = 2;
+// let replacenum = 1;
+
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i] === num1){
+//         arr[i] = replacenum;        
+//     }
+// }
+
+// console.log(arr);
+
+
+// find the sum of all number in arr
+
+
+// let arr = [3,1,2,5,7];
+
+// let sum = 0;
+
+// for(let i = 0; i<arr.length;i++){
+//         sum = arr[i]+sum;
+// }
+// console.log(sum);
+
+// let runs = [4,2,3,-1,0,2,-1,2];
+
+// let ball = 5;
+// let missingball = 0;
+
+// for(let i = 0;i<runs.length;i++){
+//     if(runs[i] == -1){
+//         missingball++
+//     }
+// }
+// console.log(ball-missingball);
